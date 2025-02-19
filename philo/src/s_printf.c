a305c1638bb0dca9b6dc529db23141c940903d06
Aisling <aisling.fontaine@protonmail.com>
2025-02-19 14:16:42 +0100

    lay out an idea for printf protection

---
+++ philo/src/s_printf.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   s_printf.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 14:08:23 by estettle          #+#    #+#             */
+/*   Updated: 2025/02/19 14:16:18 by estettle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	s_printf(char *format, ...)
+{
+	// lock stdout mutex before using printf and writing to stdout
+	// write stuff
+	// unlock stdout mutex
+}
